9b6776829188dbde7bf07ecf55d35548877c878e
flr45 <151404060+flr45@users.noreply.github.com>
2026-08-01 14:58:14 +0200

    Tillad deaktivering af formularens submitknap

--- src/components/SubmitButton.tsx
+++ src/components/SubmitButton.tsx
@@ -4,17 +4,19 @@ import { useFormStatus } from "react-dom";
 
 export function SubmitButton({
   children,
-  pendingText = "Gemmer..."
+  pendingText = "Gemmer...",
+  disabled = false
 }: {
   children: React.ReactNode;
   pendingText?: string;
+  disabled?: boolean;
 }) {
   const { pending } = useFormStatus();
 
   return (
     <button
       className="app-button-primary w-full sm:w-auto"
-      disabled={pending}
+      disabled={disabled || pending}
       type="submit"
     >
       {pending ? pendingText : children}
